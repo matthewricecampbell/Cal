4d4daf825c54a373f12dcad7a4c6372810d07764
Matt Campbell <matthewricecampbell@gmail.com>
2012-10-01 11:50:44 -0500

    addition to code notes

--- cal /lib/cal.rb
+++ cal /lib/cal.rb
@@ -15,7 +15,7 @@ year = ARGV[1]
 get_calendar_for_month_year(month, year)
 puts "Su Mo Tu We Th Fr Sa"
 y = start_day_spacing(month, year).to_s + day_of_the_week_printer(month, year)
-puts y.scan(/.{1,21}/)
+puts y.scan(/.{1,21}/)#(scans and puts characters 1 through 21 and the starts a new line)
 
 # "123456".scan(/.../){ |x| puts x }
 # .insert(1, ' ').insert(2, ' ').insert(4, ' ').insert(5, ' ').insert(7, ' ').insert(8, ' ').insert(10, ' ').insert(11, ' ').insert(13, ' ').insert(14, ' ').insert(16, ' ').insert(17, ' ').insert(19, ' ').insert(20, ' ').insert(22, ' ').insert(23, ' ').insert(25, ' ').insert(28, ' ').insert(31, ' ').insert(34, ' ').insert(37, ' ').insert(40, ' ').insert(43, ' ').insert(46, ' ').insert(49, ' ').insert(52, ' ').insert(55, ' ').insert(58, ' ').insert(61, ' ').insert(64, ' ').insert(67, ' ').insert(70, ' ').insert(73, ' ').insert(76, ' ').insert(79, ' ').insert(82, ' ').insert(85, ' ').insert(88, ' ')
--- cal /lib/calendar.rb
+++ cal /lib/calendar.rb
@@ -5,17 +5,20 @@ def get_month_name(month_number)
 	month_number_hash[month_number]
 end
 #feb_leap_year finds out if it is leap year and coresponds to the days_in_month_hash (ie 02 =>(feb_leap_year(year) ? 29 : 28))
+# if it's a leap year this method will return true
 def feb_leap_year(year)
 	year = year.to_i
 	if year%400 == 0 || year%4 == 0 && year%100!=0 
 		return true
     end
 end
+#number_of_days_in_month(month_number, year) determines days in given arguments month
 def number_of_days_in_month(month_number, year)
 	month_number = month_number.to_i
 	days_in_month_hash = {01 => 31, 02 =>(feb_leap_year(year) ? 29 : 28), 03 => 31, 04 => 30, 05 => 31, 06 => 30, 07 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
     days_in_month_hash[month_number]
 end
+#day_of_the_week_printer(month, year) returns output of days of week (ie a string of "1234567891011" etc.) 
 def day_of_the_week_printer(month, year)
 	output = ""
 	1.upto(number_of_days_in_month(month, year)) {|number| 
@@ -29,7 +32,7 @@ def day_of_the_week_printer(month, year)
 	}
 	return output
 end
-
+# jan_feb_year_offset(month, year) subtracts 1 from year if month is jan or feb(this relates to zellers)
 def jan_feb_year_offset(month, year)
 	month = month.to_i
 	year = year.to_i
@@ -39,27 +42,32 @@ def jan_feb_year_offset(month, year)
 		year
 	end
 end
+#note zellers takes jan as 13 and feb as 14
 def zeller_month_number(month_number)
 	month_number = month_number.to_i
 	converter_hash = {01=>13, 02=>14, 03=>3, 04=>4, 05=>5, 06=>6, 07=>7, 8=>8, 9=>9, 10=>10, 11=>11, 12=>12}
 	converter_hash[month_number]
 end
+# see http://en.wikipedia.org/wiki/Zeller's_congruence 
+# jan_feb_year_offset(month, year) returns the correct year (ie 2000 is 1999 or 1999 is 1999)
 def leap_year_offset(month, year) 
  (jan_feb_year_offset(month, year)/4).floor+6*(jan_feb_year_offset(month, year)/100).floor+(jan_feb_year_offset(month, year)/400).floor
 end
-
+#part of zeller's
 def march_offset(month)
  ((zeller_month_number(month.to_i)+1)*26/10).floor 
 end
+#this is the most important method we need to calculate the start day any given month
+#this is the zeller's congruence algorithem
 def start_day_of_week(month, year)
  day_of_month = 1
  (day_of_month+march_offset(month)+jan_feb_year_offset(month, year)+leap_year_offset(month, year))%7
 end
-
-
+#simple method that puts the month and year correctly spaced out
 def get_calendar_for_month_year(month, year)
   puts "    "+get_month_name(month)+" "+year
 end
+#start_day_spacing(month, year) is the padding to assure the start_day_of_week falls under the correct day (ie Sat, Sun, etc.)
 #Sat=0 ...Fri=6
 def start_day_spacing(month, year)
   if start_day_of_week(month, year) == 0
