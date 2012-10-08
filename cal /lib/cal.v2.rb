5eb8d859cdefde6f02d6e3bb7bf0c8fd0a176174
Matt Campbell <matthewricecampbell@gmail.com>
2012-10-08 13:53:18 -0500

    full year working
    need to fix the "8" after each 3 month print

--- cal /lib/cal.v2.rb
+++ cal /lib/cal.v2.rb
@@ -9,7 +9,10 @@ else
 	month = 1
 	year = ARGV[0]
 	puts "                               "+year
-	puts cal.full_year(month, year).join("\n")
+	puts cal.full_year_a(month, year)
+	puts cal.full_year_b(month, year)
+	puts cal.full_year_c(month, year)
+	puts cal.full_year_d(month, year)
 end
 end
 
@@ -30,21 +33,103 @@ def single_month_for_full_year(month, year)
 	output = []
 	output << cal.get_calendar_for_month_year(month, year)
 	output << "Su Mo Tu We Th Fr Sa "
-	padding_and_days_of_week = cal.start_day_padding(month, year).to_s + cal.days_of_the_week(month, year)
-	output << padding_and_days_of_week.scan(/.{1,21}/)
+	@padding_and_days_of_week = cal.start_day_padding(month, year).to_s + cal.days_of_the_week(month, year)
+	output << @padding_and_days_of_week.scan(/.{1,21}/)
 	output.flatten
 end
 
-def full_year(month, year)
+def full_year_a(month, year)
 	cal = Calendar.new(month, year)
 	m1 = cal.single_month_for_full_year(month, year)
 	m2 = cal.single_month_for_full_year(month+1, year)
 	m3 = cal.single_month_for_full_year(month+2, year)
+	    if m1.length < 8
+			m1 << " "*21
+			m1.flatten
+		end
+	
+		if m2.length < 8
+			m2 << " "*21
+			m2.flatten
+		end
+		 
+		if m3.length < 8
+			m3 << " "*21
+			m3.flatten
+		end
 	8.times do |i| 
-	puts m1[i]+"  "+m2[i]+"  "+m3[i]
+	puts  m1[i].ljust(21)+"  "+m2[i].ljust(21)+" "+m3[i].ljust(21)
 end
 end
-
+def full_year_b(month, year)
+	cal = Calendar.new(month, year)
+	m4 = cal.single_month_for_full_year(month+3, year)
+	m5 = cal.single_month_for_full_year(month+4, year)
+	m6 = cal.single_month_for_full_year(month+5, year)
+	if m4.length < 8
+			m4 << " "*21
+			m4.flatten
+		end
+	
+		if m5.length < 8
+			m5 << " "*21
+			m5.flatten
+		end
+		 
+		if m6.length < 8
+			m6 << " "*21
+			m6.flatten
+		end
+	8.times do |i| 
+	puts m4[i].ljust(21)+"  "+m5[i].ljust(21)+"  "+m6[i].ljust(21)
+end
+end
+def full_year_c(month, year)
+	cal = Calendar.new(month, year)
+	m7 = cal.single_month_for_full_year(month+6, year)
+	m8 = cal.single_month_for_full_year(month+7, year)
+	m9 = cal.single_month_for_full_year(month+8, year)
+	if m7.length < 8
+			m7 << " "*21
+			m7.flatten
+		end
+	
+		if m8.length < 8
+			m8 << " "*21
+			m8.flatten
+		end
+		 
+		if m9.length < 8
+			m9 << " "*21
+			m9.flatten
+		end
+	8.times do |i| 
+	puts m7[i].ljust(21)+"  "+m8[i].ljust(21)+"  "+m9[i].ljust(21)
+end
+end
+def full_year_d(month, year)
+	cal = Calendar.new(month, year)
+	m10 = cal.single_month_for_full_year(month+9, year)
+	m11 = cal.single_month_for_full_year(month+10, year)
+	m12 = cal.single_month_for_full_year(month+11, year)
+	if m10.length < 8
+			m10 << " "*21
+			m10.flatten
+		end
+	
+		if m11.length < 8
+			m11 << " "*21
+			m11.flatten
+		end
+		 
+		if m12.length < 8
+			m12 << " "*21
+			m12.flatten
+		end
+	8.times do |i| 
+	puts m10[i].ljust(21)+"  "+m11[i].ljust(21)+"  "+m12[i].ljust(21)
+end
+end
 cal_printer
 
 	
